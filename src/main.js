7be7b63f9e847faafe4a3618671743a1e7611647
Chris Johnson <cjohnson@cjoatx.com>
2025-05-11 18:59:44 -0500

    configure vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+// Install vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
