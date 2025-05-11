7d45e6ce05557694aded91d556eb6643c3f3c6ba
Chris Johnson <cjohnson@cjoatx.com>
2025-05-10 20:12:30 -0500

    updated logos

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const name = ref('Unknown')
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/j4atx-trans-logo.svg" width="350" height="350" />
 
     <div class="wrapper">
       <nav>
