8302aeaeef6a55181ea433e9d5a1bb672abb3dbe
Chris Johnson <cjohnson@cjoatx.com>
2025-05-12 20:25:19 -0500

    add fallthough route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Products from '../views/Products.vue'
+import Products from '@/views/Products.vue'
+import NotFound from '@/views/404.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       name: 'About',
       component: () => import('../views/AboutView.vue'),
     },
+    { 
+      path: '/:pathMatch(.*)*', 
+      name: 'NotFound', 
+      component: NotFound
+    }
   ],
 })
 
