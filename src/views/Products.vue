bb5bcc5d4c828e269563d805ceb1be9d168c28d4
Chris Johnson <cjohnson@cjoatx.com>
2025-05-14 10:12:37 -0500

    implemented vuetify containers

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -42,28 +42,43 @@ const decQty = (index) => {
 <template>
     <main>
         <div>
-            <v-table>
-                <tbody>
-                    <tr>
-                        <th><h2>Part</h2></th>
-                        <th><h2>Description</h2></th>
-                        <th style="text-align: center;"><h2>Quantity</h2></th>
-                    </tr>
-                    <tr v-for="(product, index) in products" :key="index">
-                        <td>{{ product.PartNum }}</td>
-                        <td>{{  product.Description }}</td>
-                        <td style="text-align: right;">{{ product.Qty }} ({{ product.Qty * product.BoxQty }}) <v-btn class="button" style="background: green;" @click="incQty(index)">+</v-btn><v-btn class="button" style="background: red; " @click="decQty(index)">-</v-btn></td>
-                    </tr>
-                </tbody>
-            </v-table>
+            <v-container fluid width="750">
+                <v-row class="cell">
+                    <v-col cols="2">Part</v-col>
+                    <v-col>Description</v-col>
+                    <v-col cols="4" class="center">Qty</v-col>
+                </v-row>
+                <v-row class="cell" v-for="( product, index ) in products" :key="index">
+                    <v-col cols="2">{{ product.PartNum }}</v-col>
+                    <v-col>{{ product.Description }}</v-col>
+                    <v-col cols="4" style="text-align: right;">{{ product.Qty }} ({{ product.Qty * product.BoxQty }}) <v-btn class="button" style="background: green;" @click="incQty(index)">+</v-btn><v-btn class="button" style="background: red; " @click="decQty(index)">-</v-btn></v-col>
+                </v-row>
+                <v-row class="lastCell">
+                    <v-col class="center" cols="3">
+                        <v-btn :disabled="!changes" @click="writeChanges">Submit changes</v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
         </div>
-        <div style="margin: 10px;">
-            <v-btn :disabled="!changes" @click="writeChanges">Submit changes</v-btn>
-        </div>
-        
     </main>
 </template>
 
 <style>
-@import '@/assets/products.css';
+/* @import '@/assets/products.css'; */
+
+.v-container {
+	text-align: left;
+}
+.cell:nth-child(odd) {
+	background: rgb(172,202,118);
+}
+.cell:nth-child(1) {
+	background: rgb(120,188,187);
+}
+.lastCell {
+	background: rgb(120,188,187);
+}
+.center {
+	text-align: center;
+}
 </style>
