3ac867a13f17f8114fd94879eb7788c97c697674
Chris Johnson <cjohnson@cjoatx.com>
2025-05-15 16:03:18 -0500

    reformat with prettier

--- server/index.ts
+++ server/index.ts
@@ -1,55 +1,72 @@
 import { Hono } from 'hono'
 
 type Bindings = {
-  // @ts-ignore
-  DB : D1Database
+	// @ts-ignore
+	DB: D1Database
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/api/products', async (c) => {
-  const resp = await c.env.DB.prepare("select * from nxstage;").all()  
-  const products = resp.results
+	const resp = await c.env.DB.prepare('select * from nxstage;').all()
+	const products = resp.results
 
-  return c.json(products)
+	return c.json(products)
 })
 
 // ToDo update to be a batch function
 app.put('/api/products', async (c) => {
-  const query = c.req.query()
-  for (const [key, value] of Object.entries(query)) {
-    await c.env.DB.prepare(
-					"UPDATE NxStage SET Qty = ? WHERE Name = ?"
-				).bind(value, key).run();
-  }
-  return new Response(null, { status: 200 });
+	const query = c.req.query()
+	for (const [key, value] of Object.entries(query)) {
+		await c.env.DB.prepare('UPDATE NxStage SET Qty = ? WHERE Name = ?')
+			.bind(value, key)
+			.run()
+	}
+	return new Response(null, { status: 200 })
 })
 
 app.post('/api/product', async (c) => {
-  const body = await c.req.json()
-  console.log(body.ID, body.PartNum, body.Name, body.Description, body.Qty, body.BoxQty)
+	const body = await c.req.json()
+	console.log(
+		body.ID,
+		body.PartNum,
+		body.Name,
+		body.Description,
+		body.Qty,
+		body.BoxQty
+	)
 
-  if (typeof body.ID == "number") {
-    const resp = await c.env.DB.prepare( `UPDATE NxStage
-      SET PartNum = ?, Name = ?, Description = ?, Qty = ?, BoxQty = ? WHERE ID = ? RETURNING *`)
-      .bind(body.PartNum, body.Name, body.Description, body.Qty, body.BoxQty, body.ID)
-      .run()
+	if (typeof body.ID == 'number') {
+		const resp = await c.env.DB.prepare(
+			`UPDATE NxStage
+      SET PartNum = ?, Name = ?, Description = ?, Qty = ?, BoxQty = ? WHERE ID = ? RETURNING *`
+		)
+			.bind(
+				body.PartNum,
+				body.Name,
+				body.Description,
+				body.Qty,
+				body.BoxQty,
+				body.ID
+			)
+			.run()
 
-    // return new Response(null, { status: 200 });
-    console.log(resp.results)
-    const ok = resp.success
-    return c.json({ok})
-  } 
+		// return new Response(null, { status: 200 });
+		console.log(resp.results)
+		const ok = resp.success
+		return c.json({ ok })
+	}
 
-  const resp = await c.env.DB.prepare(
-    `INSERT INTO NxStage (PartNum, Name, Description, Qty, BoxQty) VALUES (?, ?, ?, ?, ?) RETURNING *`)
-    .bind(body.PartNum, body.Name, body.Description, body.Qty, body.BoxQty)
-    .run()
+	const resp = await c.env.DB.prepare(
+		`INSERT INTO NxStage (PartNum, Name, Description, Qty, BoxQty) VALUES (?, ?, ?, ?, ?) RETURNING *`
+	)
+		.bind(body.PartNum, body.Name, body.Description, body.Qty, body.BoxQty)
+		.run()
 
-  // return new Response(null, { status: 200 });
-  console.log(resp.results)
-  const ok = resp.success
-  return c.json({ok})
+	// return new Response(null, { status: 200 });
+	console.log(resp.results)
+	const ok = resp.success
+	return c.json({ ok })
 })
 
 export default app
