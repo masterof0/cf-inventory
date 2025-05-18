992aeff88aa9d0da4840b576d740c985c5992061
Chris Johnson <cjohnson@cjoatx.com>
2025-05-18 15:35:14 -0500

    configure and authenticate with supabase

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,46 @@
+<script setup>
+import { reactive } from 'vue'
+import { createClient } from '@supabase/supabase-js'
+import { mdiLogin } from '@mdi/js';
+
+const supaurl = import.meta.env.VITE_SUPAURL;
+const supaapi = import.meta.env.VITE_SUPAAPI;
+const supabase = createClient(supaurl, supaapi);
+
+const account = reactive({
+  email: "",
+  password: ""
+})
+
+const login = async () => {
+  await supabase.auth.signInWithPassword({
+    email: account.email,
+    password: account.password,
+  })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+
+  account.password = ""
+}
+
+</script>
 <template>
-  <div class="about">
-    <h1>This is the login page</h1>
+  <div>
+    <v-container width="500">
+      <v-card>
+        <v-toolbar color="blue"><v-toolbar-title>Login</v-toolbar-title></v-toolbar>
+      </v-card>
+      <v-card-text>
+        <v-text-field v-model="account.email" label="email" name="email" autocomplete="on"></v-text-field>
+        <v-text-field v-model="account.password" type="password" label="password" name="password"></v-text-field>
+        <v-icon :icon="mdiLogin" @click="login"></v-icon>
+      </v-card-text>
+    </v-container>
   </div>
 </template>
 
-<style>
-.about {
-  text-align: center;
-}
-</style>
+<style></style>
