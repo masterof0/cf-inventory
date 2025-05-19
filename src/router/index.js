531c845f080a0cccf0898bde860bd400432772b4
Chris Johnson <cjohnson@cjoatx.com>
2025-05-19 15:29:49 -0500

    split routing for 404

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import { supabase } from '@/services/supabase'
 import Products from '@/views/Products.vue'
 import Manage from '@/views/Manage.vue'
 import Login from '@/views/Login.vue'
-import NotFound from '@/views/404.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,15 +23,16 @@ const router = createRouter({
 			component: Manage,
 			meta: { requiresAuth: true },
 		},
-		{
-			path: '/about',
-			name: 'About',
-			component: () => import('../views/AboutView.vue'),
-		},
+		// {
+		// 	path: '/about',
+		// 	name: 'About',
+		// 	component: () => import('../views/AboutView.vue'),
+		// },
 		{
 			path: '/:pathMatch(.*)*',
 			name: 'NotFound',
-			component: NotFound,
+			component: () => import('@/views/404.vue'),
+			// component: NotFound,
 		},
 	],
 })
