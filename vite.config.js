a8e4c120006031b3e617ad42d66d05867f46a5a7
masterof0 <cjohnson@cjoatx.com>
2025-05-28 11:55:33 -0500

    install tailwind

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 import { cloudflare } from "@cloudflare/vite-plugin"
 
@@ -11,6 +12,7 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		vueDevTools(),
+		tailwindcss(),
 		cloudflare()
 	],
 	resolve: {
